7b1b2a50ca3a44f47fcf9ad778169cf62d8d1ace
jd richards <jdrichardstech@gmail.com>
2023-01-20 15:19:03 -0500

    remove server.js

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 // DEPENDENCIES
-const app = require("./app.js");
 
 // CONFIGURATION
-require("dotenv").config();
 
-const PORT = process.env.PORT;
 
 // LISTEN
-app.listen(PORT, () => {
-  console.log(`🥤 🍿 Snackin' on port ${PORT} 🥨 🌰 `);
-});
