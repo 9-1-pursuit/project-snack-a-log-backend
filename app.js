ff1f07758512a13abf2af130f3231bf2ef41c61d
TrianePeart <TrianePeart@gmail.com>
2023-01-27 14:47:23 -0500

    inital commit (1.27.23)

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 // DEPENDENCIES
-
-
+const express = require("express");
+const cors = require("cors");
+const snackController = require("./controllers/snackController")
 
 // CONFIGURATION
-
+const app = express()
 
 // MIDDLEWARE
+app.use(express.json());
+app.use(cors()); 
+// app.use("/snacks", snackController);
+
 
 // ROUTES
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to the snack app");
+  });
+  
+  app.get("/not-found", (req, res) => {
+    res.status(404).json({ error: "page does not exist" });
+  });
+  
+  app.get("*", (req, res) => {
+    res.redirect("/not-found");
+  });
 
 // EXPORT
-
+module.exports = app;
--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,12 @@ CREATE DATABASE snack_a_log;
 
 \c snack_a_log; 
 
-
+CREATE TABLE snacks (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    fiber NUMERIC DEFAULT 0 NOT NULL,
+    protein NUMERIC DEFAULT 0 NOT NULL,
+    added_sugar NUMERIC DEFAULT 0,
+    is_healthy BOOLEAN,
+    image TEXT DEFAULT 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'
+); 
