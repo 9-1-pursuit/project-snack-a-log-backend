90c6f435c5e776c4ea2d3f143b4bd2654c71c0da
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-01-30 12:04:12 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 // DEPENDENCIES
-
-
+const cors = require('cors');
+const express = require('express');
+const snackController = require('./controllers/snackController');
 
 // CONFIGURATION
-
+const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// Snack ROUTES
+app.use('/snacks', snackController);
 
 // ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to Snack-A-Log');
+});
 
+// Error
+app.get('*', (req, res) => {
+  res.status(404).send('Page does not exist');
+});
 // EXPORT
-
+module.exports = app;
