8bbf36fcaf1653da521aaeb65c816dfa766159e5
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-01-30 11:58:27 -0500

    snack js updated

--- queries/snacks.js
+++ queries/snacks.js
@@ -1,3 +1,80 @@
-const db = require("../db/dbConfig.js");
+const db = require('../db/dbConfig.js');
 
-module.exports = {};
+const getAllSnacks = async (snackId) => {
+  try {
+    const allSnacks = await db.any(
+      'SELECT * FROM snacks WHERE snack_id = $1',
+      snackId
+    );
+    return allSnacks;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getSnack = async (id) => {
+  try {
+    const snack = await db.one('SELECT * FROM snacks WHERE id=$1', id);
+    return snack;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createSnack = async (snack) => {
+  try {
+    const newReview = await db.one(
+      'INSERT INTO snacks (name,fiber,protein, added_sugar, isHealthy,image) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
+      [
+        snack.name,
+        snack.fiber,
+        snack.protein,
+        snack.added_sugar,
+        snack.isHealthy,
+        snack.image,
+      ]
+    );
+    return newReview;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteSnack = async (id) => {
+  try {
+    const deletedSnack = await db.one(
+      'DELETE FROM snacks WHERE id=$1 RETURNING *',
+      id
+    );
+    return deletedSnack;
+  } catch (error) {
+    return error;
+  }
+};
+const updateSnack = async (id, snack) => {
+  try {
+    const updatedSnack = await db.one(
+      'UPDATE snacks SET name=$1, fiber=$2, protein=$3, added_sugar=$4, snack.isHealthy= $5, snack.image=$6 WHERE id=$7 RETURNING *',
+      [
+        snack.name,
+        snack.fiber,
+        snack.protein,
+        snack.added_sugar,
+        snack.isHealthy,
+        snack.image,
+        id,
+      ]
+    );
+    return updatedSnack;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getAllSnacks,
+  getSnack,
+  createSnack,
+  deleteSnack,
+  updateSnack,
+};
